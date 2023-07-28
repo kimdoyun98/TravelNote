b17cdd762c8c1991d8eb9721663dc28a851e0f02
kimdoyun98 <rladlsxo1250@gmail.com>
2023-07-29 04:18:22 +0900

    jetpack-bottom-nav & toolbar & binding
    초기셋팅

---
+++ android/app/src/main/java/com/example/android/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.main_fragment)
+
+        binding.navBar.setupWithNavController(navController)
+    }
+}
