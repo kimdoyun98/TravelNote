919d2695a33c7ee927d93620d77a3a670d2aa866
kimdoyun98 <rladlsxo1250@gmail.com>
2023-10-19 01:06:55 +0900

    Delete Secret.kt

--- android/app/src/main/java/com/example/android/Secret.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android
-
-object Secret {
-    const val BaseUrl = "http://192.168.219.102:8000"
-    const val AddressSecretKey = "devU01TX0FVVEgyMDIzMDkxNjE3MzQwNDExNDEwODc="
-    const val AddressUrl = "https://business.juso.go.kr/addrlink/"
-}
